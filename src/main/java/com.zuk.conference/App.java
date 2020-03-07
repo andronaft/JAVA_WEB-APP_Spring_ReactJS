56b6839c4438946705a135124c2f601b318d3c5b
andronaft <andronaft99@gmail.com>
2020-03-07 03:27:03 +0200

    try to deploy heroku 0_2

--- src/main/java/com.zuk.conference/App.java
+++ src/main/java/com.zuk.conference/App.java
@@ -1,5 +1,9 @@
 package com.zuk.conference;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
